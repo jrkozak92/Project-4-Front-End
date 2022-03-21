06d3179448c6d8bdbbe49515c75455c82a0f8790
Roc Taylor <rocrtaylor@gmail.com>
2022-03-21 09:21:16 -0500

    foldable with Joey

--- src/components/MapColumn.js
+++ src/components/MapColumn.js
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import Edit from "./Edit";
 import "../App.css";
 
 const MapColumn = (props) => {
+  const [showForm, setShowForm] = useState();
+
+  const toggleShowForm = (todoId) => {
+    if (todoId === showForm) {
+      setShowForm("");
+    } else {
+      setShowForm(todoId);
+    }
+  };
+
   return (
     <div className="mapColumn">
       <h2 className="lid">{props.title}</h2>
       {props.todos.map((todo, i) => {
         return (
           <div className="todoCard" key={i}>
-            <h5 style={{ fontSize: "1.5rem" }}>{todo.title}</h5>
+            <h5
+              onClick={() => {
+                toggleShowForm(todo.id);
+              }}
+              style={{ fontSize: "1.5rem" }}
+            >
+              {todo.title}
+            </h5>
             <p>{todo.task}</p>
-            <Edit todo={todo} handleUpdate={props.handleUpdate} />
-            <button onClick={props.handleDelete} value={todo.id} style={{ margin: "1.25rem", backgroundColor: "rgb(19, 39, 67)", color: "rgb(215,56,94", borderRadius: "15px", fontSize: "1rem", padding: "4px 7px" }}>
-              {" "}
-              Delete
-            </button>
+            {showForm === todo.id ? (
+              <>
+                <Edit todo={todo} handleUpdate={props.handleUpdate} />
+                <button onClick={props.handleDelete} value={todo.id} style={{ margin: "1.25rem", backgroundColor: "rgb(19, 39, 67)", color: "rgb(215,56,94", borderRadius: "15px", fontSize: "1rem", padding: "4px 7px" }}>
+                  {" "}
+                  Delete
+                </button>
+              </>
+            ) : null}
           </div>
         );
       })}
