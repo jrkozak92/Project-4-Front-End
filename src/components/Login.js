9ace3de09273b8ef5d5ecac1faa6f7560279403a
jrkozak92 <jrkozak92@gmail.com>
2022-03-21 17:55:47 -0500

    Changed Login input password to password field for non-create

--- src/components/Login.js
+++ src/components/Login.js
@@ -117,7 +117,7 @@ const Login = (props) => {
               <h5>Login</h5>
               <form onSubmit={handleLogin}>
                 <input type="text" name="username" placeholder="Username" onChange={handleChange} style={{ backgroundColor: "rgb(248,239,212)", marginRight: "5px", border: "1.5px solid rgb(215,56,94)", borderRadius: "5px" }} />
-                <input type="text" name="password" placeholder="Password" onChange={handleChange} style={{ backgroundColor: "rgb(248,239,212)", marginLeft: "5px", border: "1.5px solid rgb(215,56,94)", borderRadius: "5px" }} />
+                <input type="password" name="password" placeholder="Password" onChange={handleChange} style={{ backgroundColor: "rgb(248,239,212)", marginLeft: "5px", border: "1.5px solid rgb(215,56,94)", borderRadius: "5px" }} />
                 <p>{props.loginMessage}</p>
                 <input type="submit" value="Login" style={{ backgroundColor: "rgb(237,201,136)", color: "rgb(19,39,67)", borderRadius: "15px", marginBottom: "10px", fontSize: "1rem", padding: "4px 7px" }} />
               </form>
